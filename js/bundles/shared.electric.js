dd171c0453d564b33fd6a611978f3d3cdf3b201d
Wilson Miao Liu <lockleeliumiaowilson@gmail.com>
2018-08-09 10:13:26 +1000

    update docs

--- js/bundles/shared.electric.js
+++ js/bundles/shared.electric.js
@@ -34,7 +34,7 @@
 /******/
 /******/ 	// objects to store loaded and loading chunks
 /******/ 	var installedChunks = {
-/******/ 		47: 0
+/******/ 		48: 0
 /******/ 	};
 /******/
 /******/ 	// The require function
---
+++ js/bundles/wqCZC.js
@@ -0,0 +1,231 @@
+var pageComponent =
+webpackJsonppageComponent([12],{
+
+/***/ 338:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _metalComponent = __webpack_require__(0);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(1);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+__webpack_require__(4);
+
+__webpack_require__(5);
+
+__webpack_require__(6);
+
+__webpack_require__(7);
+
+__webpack_require__(8);
+
+__webpack_require__(9);
+
+__webpack_require__(10);
+
+__webpack_require__(11);
+
+__webpack_require__(12);
+
+__webpack_require__(13);
+
+__webpack_require__(14);
+
+__webpack_require__(15);
+
+var _template_stringSoy = __webpack_require__(339);
+
+var _template_stringSoy2 = _interopRequireDefault(_template_stringSoy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var wqCZC = function (_Component) {
+  _inherits(wqCZC, _Component);
+
+  function wqCZC() {
+    _classCallCheck(this, wqCZC);
+
+    return _possibleConstructorReturn(this, (wqCZC.__proto__ || Object.getPrototypeOf(wqCZC)).apply(this, arguments));
+  }
+
+  return wqCZC;
+}(_metalComponent2.default);
+
+;
+
+_metalSoy2.default.register(wqCZC, _template_stringSoy2.default);
+
+exports.default = wqCZC;
+
+/***/ }),
+
+/***/ 339:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.templates = exports.wqCZC = undefined;
+
+var _metalComponent = __webpack_require__(0);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(1);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /* jshint ignore:start */
+
+
+var templates;
+goog.loadModule(function (exports) {
+
+  // This file was automatically generated from template_string.soy.
+  // Please don't edit this file by hand.
+
+  /**
+   * @fileoverview Templates in namespace wqCZC.
+   * @public
+   */
+
+  goog.module('wqCZC.incrementaldom');
+
+  /** @suppress {extraRequire} */
+  var soy = goog.require('soy');
+  /** @suppress {extraRequire} */
+  var soydata = goog.require('soydata');
+  /** @suppress {extraRequire} */
+  goog.require('goog.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('soy.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('goog.i18n.bidi');
+  /** @suppress {extraRequire} */
+  goog.require('goog.string');
+  var IncrementalDom = goog.require('incrementaldom');
+  var ie_open = IncrementalDom.elementOpen;
+  var ie_close = IncrementalDom.elementClose;
+  var ie_void = IncrementalDom.elementVoid;
+  var ie_open_start = IncrementalDom.elementOpenStart;
+  var ie_open_end = IncrementalDom.elementOpenEnd;
+  var itext = IncrementalDom.text;
+  var iattr = IncrementalDom.attr;
+
+  var $templateAlias2 = _metalSoy2.default.getTemplate('ElectricCode.incrementaldom', 'render');
+
+  var $templateAlias1 = _metalSoy2.default.getTemplate('guide.incrementaldom', 'render');
+
+  /**
+   * @param {{
+   *    page: (?),
+   *    site: (?)
+   * }} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $render(opt_data, opt_ignored, opt_ijData) {
+    var param1065 = function param1065() {
+      ie_open('h6');
+      var dyn62 = opt_data.page.description;
+      if (typeof dyn62 == 'function') dyn62();else if (dyn62 != null) itext(dyn62);
+      ie_close('h6');
+      ie_open('article', null, null, 'id', '1');
+      ie_open('h2');
+      itext('Feature Overview');
+      ie_close('h2');
+      ie_open('p');
+      itext('This feature converts a template string into concatenated strings.');
+      ie_close('p');
+      ie_close('article');
+      ie_open('article', null, null, 'id', '2');
+      ie_open('h2');
+      itext('Prerequisite');
+      ie_close('h2');
+      ie_open('p');
+      itext('None');
+      ie_close('p');
+      ie_close('article');
+      ie_open('article', null, null, 'id', '3');
+      ie_open('h2');
+      itext('Sweet Apex Example');
+      ie_close('h2');
+      $templateAlias2({ code: 'public class TemplateStringDemo {\n    public static String a = \'a\';\n    public static String b = \'b\';\n    public static String c = `${a}-${b}`;\n    public static String text = `\n        This is a free style text.\n        You can add ${c} here.\n        Try it.\n        `;\n}', mode: 'javascript' }, null, opt_ijData);
+      ie_close('article');
+      ie_open('article', null, null, 'id', '4');
+      ie_open('h2');
+      itext('Transpiled Apex');
+      ie_close('h2');
+      $templateAlias2({ code: 'public class TemplateStringDemo {\n    public static String a = \'a\';\n    public static String b = \'b\';\n    public static String c = \'\' + a + \'-\' + b + \'\';\n    public static String text = \'\\n        This is a free style text.\\n        You can add \' + c + \' here.\\n        Try it.\\n        \';\n}', mode: 'javascript' }, null, opt_ijData);
+      ie_close('article');
+      ie_open('article', null, null, 'id', '5');
+      ie_open('h2');
+      itext('Usage');
+      ie_close('h2');
+      ie_close('article');
+      ie_open('input', null, null, 'type', 'hidden', 'value', opt_data.page.title);
+      ie_close('input');
+      ie_open('input', null, null, 'type', 'hidden', 'value', opt_data.site.title);
+      ie_close('input');
+    };
+    $templateAlias1(soy.$$assignDefaults({ content: param1065 }, opt_data), null, opt_ijData);
+  }
+  exports.render = $render;
+  if (goog.DEBUG) {
+    $render.soyTemplateName = 'wqCZC.render';
+  }
+
+  exports.render.params = ["page", "site"];
+  exports.render.types = { "page": "?", "site": "?" };
+  exports.templates = templates = exports;
+  return exports;
+});
+
+var wqCZC = function (_Component) {
+  _inherits(wqCZC, _Component);
+
+  function wqCZC() {
+    _classCallCheck(this, wqCZC);
+
+    return _possibleConstructorReturn(this, (wqCZC.__proto__ || Object.getPrototypeOf(wqCZC)).apply(this, arguments));
+  }
+
+  return wqCZC;
+}(_metalComponent2.default);
+
+_metalSoy2.default.register(wqCZC, templates);
+exports.wqCZC = wqCZC;
+exports.templates = templates;
+exports.default = templates;
+/* jshint ignore:end */
+
+/***/ })
+
+},[338]);
